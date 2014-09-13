6bc083be41ace52909e2c7aece48c9794907f9d0
Dennis Sutch <dennis@sutch.com>
2014-09-12 23:11:18 -0400

    Remove redundant apt-get update

--- script/gnome.sh
+++ script/gnome.sh
@@ -2,9 +2,6 @@
 
 # Setup  KDE
 
-sudo apt-get --assume-yes update
-#sudo apt-get --assume-yes install kubuntu-desktop --no-install-recommends
-
 sudo apt-get --assume-yes install xorg gnome-core gnome-system-tools gnome-app-install
 
 reboot
